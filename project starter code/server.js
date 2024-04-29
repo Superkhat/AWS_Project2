d9be64df7d994a7c2b50a45908393f0f5823b4e1
ThanhNC <supertato@SuperTatos-MacBook-Pro.local>
2024-04-29 10:04:24 +0700

    update code

--- project starter code/server.js
+++ project starter code/server.js
@@ -11,7 +11,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+  app.use(bodyParser.json());   
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
